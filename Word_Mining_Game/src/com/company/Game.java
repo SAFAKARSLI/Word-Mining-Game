c84f14d87c7c560eca2c47bd3771fea7cca7fae9
furkanaktepe01 <7.furkanaktepe@gmail.com>
2021-06-14 14:16:17 +0300

    mehmet

--- Word_Mining_Game/src/com/company/Game.java
+++ Word_Mining_Game/src/com/company/Game.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Game {
 
-    private int furkan;
+    private int mehmet;
     private ArrayList<Character> vowels;
     private ArrayList<Character> consonants;
     boolean isEnglish;
